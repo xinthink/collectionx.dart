9f0c98030ab818f30b5ab8f657e7db8910d0e015
Think Wu <yingxinwu.g@gmail.com>
2020-01-15 09:44:23 +0800

    update example

--- example/main.dart
+++ example/main.dart
@@ -6,7 +6,7 @@ void main() {
   mapsExample();
 }
 
-/// Example usages of extensions to [Iterable]s.
+/// Example usage of extensions to [Iterable]s.
 void iterablesExample() {
   // tests if all the numbers are even
   final isEven = [5, 19, 2].all((n) => n.isEven);
@@ -17,9 +17,9 @@ void iterablesExample() {
   [5, 6, 7].forEachIndexed((i, n) => indexes += '$i');
   print('indexes walked through: $indexes'); // => '012'
 
-  // `fold` the list from end to head
+  // `fold` the list in reverse direction
   final subRight = [1, 2, 3, 4].foldRight(0, subtract); // => (1 - (2 - (3 - (4 - 0)))) => -2
-  final subLeft = [1, 2, 3, 4].fold(0, subtract);
+  final subLeft = [1, 2, 3, 4].fold(0, subtract); // => ((((0 - 1) - 2) - 3) - 4) => -10
   print('[1, 2, 3, 4].foldRight(0, subtract) => $subRight, while `fold` => $subLeft');
 
   // duplicates each element using `flatMap`
@@ -30,14 +30,14 @@ void iterablesExample() {
   final parts = [1, 0, -3, 4, -6].partition((n) => n.isNegative); // ([-3, -6], [1, 0, 4])
   print('split [1, 2, -3, 4, -6] by sign => $parts');
 
-  // chunks a list into subsets with length of 2
+  // chunks a list into subsets with maximum length of 2
   final chunks = [1, 0, -3, 4, -6].chunked(2); // [[1, 0], [-3, 4], [-6]]
   print('chunks [1, 2, -3, 4, -6] by length of 2 => $chunks');
 }
 
-/// Example usages of extensions to [Map]s.
+/// Example usage of extensions to [Map]s.
 void mapsExample() {
-  // turns a sequence of key/value into a map
+  // turns a sequence of key/value pairs (a key followed by a value) into a map
   final map = mapFromList<int, String>([10, 'ten', -3, 'minus three']);
   print('map built from a list => $map');
 
